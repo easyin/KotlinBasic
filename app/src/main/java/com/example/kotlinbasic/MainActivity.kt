ca8ec9fbaca3a1daa526b95923c52d4ec71a5ec9
이지인 <wldls3521@gmail.com>
2021-02-07 16:29:22 +0900

    코틀린확장기능설치

--- app/src/main/java/com/example/kotlinbasic/MainActivity.kt
+++ app/src/main/java/com/example/kotlinbasic/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.kotlinbasic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        sendBtn.setOnClickListener(View.OnClickListener {
+            target.setText(input.text)
+        })
     }
 }
